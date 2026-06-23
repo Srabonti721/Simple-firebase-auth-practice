944f9b4ec7fa7d84e0fc482a28e019e10599c5b7
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-06-23 22:56:46 +0600

    added router and simple firebase

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>please Sign in </h1>
+            <button>Sign in With google</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './Layouts/Root';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:Root,
+    children:[
+      {
+        index:true,
+        Component:Home
+      },
+      {
+        path:"login",
+        Component:Login
+      }
+    ]
+  },
+]);
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
